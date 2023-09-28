bf0b6c917996e1975cb6846b9c7aba13fe316523
þorvaldur <breki121212@gmail.com>
2023-09-28 11:37:03 +0000

    28/09/2023

--- domVerk5/main.js
+++ domVerk5/main.js
@@ -1,53 +1,68 @@
-// Get references to the HTML elements
-const itemsList = document.getElementById("items");
-const addButton = document.getElementById("add");
-const removeButton = document.getElementById("remove");
+document.addEventListener("DOMContentLoaded", function () {
+  // Get references to the HTML elements
+  const itemsList = document.getElementById("items");
+  const addButton = document.getElementById("add");
+  const removeButton = document.getElementById("remove");
 
-// reference to html element for total price and count of items
-const totalpricehtml = document.getElementById("totalprice");
-const count = document.getElementById("count");
+  // reference to the HTML element for total price and count of items
+  const totalpricehtml = document.getElementById("totalprice");
+  const count = document.getElementById("count");
 
-const listtotalprice = [];
+  const listtotalprice = [];
 
-// Initialize an array to store the shopping list items
-const shoppingList = {
-  items: [
-    { name: "pizza", price: 850 },
-    { name: "t-shirt", price: 3000 },
-    { name: "bounty", price: 150 },
-    { name: "coke", price: 250 }
-  ],
-  total: 0
-};
+  // Initialize an array to store the shopping list items
+  let shoppingList = {
+    items: [], // Initialize as an empty array
+    total: 0
+  };
 
-shoppingList.total = shoppingList.items.length;
-
-// Function to add an item to the shopping list
-function addItem() {
-  const newItemText = prompt("Enter the item you want to add:");
-  const newItemPrice = parseFloat(prompt("Enter the price of the item you want to add:"));
-
-  if (newItemText && !isNaN(newItemPrice)) {
-    // Add the item to the list and the array
-    const newItem = { name: newItemText, price: newItemPrice };
-    shoppingList.items.push(newItem);
-    shoppingList.total++;
-    displayItems();
+  // Function to retrieve items from local storage and initialize shoppingList.items
+  function initializeItemsFromLocalStorage() {
+    const storedItems = localStorage.getItem("items");
+    if (storedItems) {
+      shoppingList.items = JSON.parse(storedItems);
+      shoppingList.total = shoppingList.items.length;
+    }
   }
-}
 
-// Function to remove the last item from the shopping list
-function removeItem() {
-  if (shoppingList.total > 0) {
-    const newRemoveText = prompt("Enter the item you want to remove:");
-    console.log("keyrt hefur verið remove fallið");
-    // Find the index of the item in the array
-    const index = shoppingList.items.findIndex(item => item.name === newRemoveText);
+  // Call the initialization function when the page loads
+  initializeItemsFromLocalStorage();
+  displayItems();
 
-    if (index !== -1) {
-      // Remove the item from the list and the array
-      shoppingList.items.splice(index, 1);
-      shoppingList.total--;
+  // ... rest of your code ...
+
+  // Event listeners for the Add and Remove buttons
+  addButton.addEventListener("click", addItem);
+  removeButton.addEventListener("click", removeItem);
+
+  // Function to add an item to the shopping list
+  function addItem() {
+    const newItemText = prompt("Enter the item you want to add:");
+    const newItemPrice = parseFloat(prompt("Enter the price of the item you want to add:"));
+
+    if (newItemText && !isNaN(newItemPrice)) {
+      // Add the item to the list and the array
+      const newItem = { name: newItemText, price: newItemPrice };
+      shoppingList.items.push(newItem);
+      shoppingList.total++;
+      localStorage.setItem("items", JSON.stringify(shoppingList.items)); // Update local storage
+      displayItems();
+    }
+  }
+
+  // Function to remove the last item from the shopping list
+  function removeItem() {
+    if (shoppingList.total > 0) {
+      const newRemoveText = prompt("Enter the item you want to remove:");
+      console.log("keyrt hefur verið remove fallið");
+      // Find the index of the item in the array
+      const index = shoppingList.items.findIndex(item => item.name === newRemoveText);
+
+      if (index !== -1) {
+        // Remove the item from the list and the array
+        shoppingList.items.splice(index, 1);
+        shoppingList.total--;
+        localStorage.setItem("items", JSON.stringify(shoppingList.items)); // Update local storage
         displayItems();
       } else {
         alert("Item not found in the list.");
@@ -55,34 +70,31 @@ function removeItem() {
     }
   }
 
-// Function to calculate the total price
-function totalprice() {
-  let listtotal = 0;
-  for (let i = 0; i < shoppingList.items.length; i++) {
-    listtotal += shoppingList.items[i].price;
-  }
-  return listtotal;
-}
-
-// Function to display the items in the shopping list
-function displayItems() {
-  itemsList.innerHTML = "";
-  for (const item of shoppingList.items) {
-    const listItem = document.createElement("li");
-    listItem.textContent = `${item.name} - $${item.price}`;
-    itemsList.appendChild(listItem);
+  // Function to calculate the total price
+  function totalprice() {
+    let listtotal = 0;
+    for (let i = 0; i < shoppingList.items.length; i++) {
+      listtotal += shoppingList.items[i].price;
+    }
+    return listtotal;
   }
 
-  // Update total price and count of items
-  listtotalprice.pop();
-  listtotalprice.push(totalprice());
-  totalpricehtml.textContent = `$${listtotalprice[0].toFixed(2)}`;
-  count.textContent = shoppingList.total;
-}
+  // Function to display the items in the shopping list
+  function displayItems() {
+    itemsList.innerHTML = "";
+    for (const item of shoppingList.items) {
+      const listItem = document.createElement("li");
+      listItem.textContent = `${item.name} - $${item.price}`;
+      itemsList.appendChild(listItem);
+    }
 
-// Initial display of items
-displayItems();
+    // Update total price and count of items
+    listtotalprice.pop();
+    listtotalprice.push(totalprice());
+    totalpricehtml.textContent = `$${listtotalprice[0].toFixed(2)}`;
+    count.textContent = shoppingList.total;
+  }
 
-// Event listeners for the Add and Remove buttons
-addButton.addEventListener("click", addItem);
-removeButton.addEventListener("click", removeItem);
+  // Initial display of items
+  displayItems();
+});
